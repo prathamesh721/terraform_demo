14a690fee0a0454bef0043f8a8ebe24e00b7fa63
prathamesh721 <prathamesh721@gmail.com>
2025-06-21 00:34:20 +0530

    Changed outDir to root

--- apps/terraform-visualizer/vite.config.ts
+++ apps/terraform-visualizer/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(() => ({
   //  plugins: [ nxViteTsPaths() ],
   // },
   build: {
-    outDir: './dist',
+    outDir: '../../dist/apps/terraform-visualizer',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
